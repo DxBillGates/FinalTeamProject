9f240dca5636822bd191f1e7a228ce45cb37cc22
DxBillGates <suzu.tomo283@gmail.com>
2022-12-31 16:47:33 +0900

    太陽の色を黒から太陽っぽく修正

--- ExeProject/Resources/Shader/CelestialSpherePixelShader.hlsl
+++ ExeProject/Resources/Shader/CelestialSpherePixelShader.hlsl
@@ -24,9 +24,12 @@ float4 main(DefaultMeshVSOutput psInput) : SV_TARGET
 	float3 defaultColor = float3(0.54f,0.76f,0.86f);
 
 	float sun = saturate(distance(sunsUV,psInput.uv) * 40);
+	float3 cream = float3(1, 0.92f, 0.7f);
+	float3 white = float3(1, 1, 1);
+	float3 sunColor = lerp(white, cream, EaseOutExpo(sun));
 	float4 texColor = tex.Sample(wrapPointSampler,psInput.uv);
 
 	float3 lightColor = float3(worldLightColor.r, worldLightColor.gb);
-	float3 resultColor = defaultColor * (lightColor * sun);
+	float3 resultColor = defaultColor * lightColor + sunColor * (1 - sun);
 	return float4(resultColor,1);
 }
