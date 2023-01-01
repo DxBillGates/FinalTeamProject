25ad2f2eefc6b85c0cf763058f38d0a17ededa73
DxBillGates <suzu.tomo283@gmail.com>
2023-01-01 17:00:57 +0900

    りふぁ

--- ExeProject/Resources/Shader/CelestialSpherePixelShader.hlsl
+++ ExeProject/Resources/Shader/CelestialSpherePixelShader.hlsl
@@ -16,23 +16,21 @@ float EaseOutExpo(float x)
 
 float4 main(DefaultMeshVSOutput psInput) : SV_TARGET
 {
-	// 太陽の高さ
-	float sunsHeight = 1 - sunsUV.y;
-	// uvの高さ
-	float uvHeight = 1 - psInput.uv.y;
+	const float3 WHITE = float3(1, 1, 1);
+	const float3 CREAM = float3(1, 0.92f, 0.7f);
 	// 高さによる色変化太陽の高さによる色変化もない状態のデフォルトカラー
-	float3 defaultColor = float3(0.44f,0.66f,1.00f);
-	float3 white = float3(1, 1, 1);
+	const float3 DEFAULT_SKY_COLOR = float3(0.44f, 0.66f, 1.00f);
+
+	// 空の表現
 	float skyValue = EaseOutExpo(psInput.uv.y);
-	float3 sky = lerp(defaultColor, white, skyValue * skyValue * skyValue);
+	float3 sky = lerp(DEFAULT_SKY_COLOR, WHITE, skyValue * skyValue * skyValue);
+
+	// 太陽の表現
 	// 太陽を表現するためのしきい値
 	float sunValue = EaseOutExpo(saturate(distance(sunsUV,psInput.uv) * 60));
-	float3 cream = float3(1, 0.92f, 0.7f);
-	float3 sunColor = lerp(white, cream, sunValue);
+	float3 sunColor = lerp(WHITE, CREAM, sunValue);
 	float3 sun = sunColor * (1 - sunValue);
 	float3 lightColor = float3(worldLightColor.rgb);
-
-	//float4 texColor = tex.Sample(wrapPointSampler,psInput.uv);
 
 	float3 resultColor = sky * lightColor + sun;
 	return float4(resultColor,1);
