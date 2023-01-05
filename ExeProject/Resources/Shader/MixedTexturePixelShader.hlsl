eb9a0ed062c77e3dc8d7a6d093cd723a7be5449b
DxBillGates <suzu.tomo283@gmail.com>
2023-01-05 15:36:05 +0900

    ふぃｘ

--- ExeProject/Resources/Shader/MixedTexturePixelShader.hlsl
+++ ExeProject/Resources/Shader/MixedTexturePixelShader.hlsl
@@ -15,11 +15,13 @@ float4 main(DefaultSpriteVSOutput input) : SV_TARGET
 {
 	float4 resultColor = float4(0,0,0,1);
 
+	float4 blur = float4(0, 0, 0, 1);
 	resultColor.rgb += defaultLayerTexture.Sample(clampLinearSampler, input.uv);
 	//resultColor.rgb += effectLayerTexture.Sample(clampLinearSampler, input.uv)/ 2;
-	resultColor.rgb += blurLayerTexture1.Sample(clampLinearSampler, input.uv) / 2;
-	resultColor.rgb += blurLayerTexture2.Sample(clampLinearSampler, input.uv) / 2;
-	resultColor.rgb += blurLayerTexture3.Sample(clampLinearSampler, input.uv) / 2;
+	blur.rgb += blurLayerTexture1.Sample(clampLinearSampler, input.uv);
+	blur.rgb += blurLayerTexture2.Sample(clampLinearSampler, input.uv);
+	blur.rgb += blurLayerTexture3.Sample(clampLinearSampler, input.uv);
+	blur.rgb /= 3;
 
-	return resultColor;
+	return float4((resultColor.rgb + blur.rgb),1);
 }
